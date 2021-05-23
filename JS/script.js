1719a1d3f446ee23fbdd01822d4544c2b0ef6c4c
Anastisia <anastasiyam1204@bk.ru>
2021-05-23 17:31:46 +0300

    Переменные

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-console */
+"use strict";
+
 let number = 5;
 const LeftBorderWidth = 1;
 
 number = 10;
 console.log(number);
 
+const array = [1, 2, 3, 4, '5', { a: '12' }, true, 'true'];
+console.dir(array);
+
 const obj = {
-    a: 50
+  a: 50,
+  b: 35,
 };
 
 obj.a = 10;
 
-console. log(obj);
+console.log(obj);
+
+// alert('Hello');
+// alert('World!');
+
+// let admin = 'Jon';
+// let name = 'Jon';
+
+// alert(admin);
+
+
