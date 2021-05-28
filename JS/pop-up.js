ce1e8f7bf2b456aac2d2d420533c22ca201a737f
Anastasia Khabarova <akhabarva@gmail.com>
2021-05-28 15:10:52 +0300

    lening popup

--- JS/pop-up.js
+++ JS/pop-up.js
@@ -1,8 +1,16 @@
 // Всплывающее окно с предупреждением
-alert('Hello World!');
+// alert('Hello World!');
 // Pop-up с вопросом
-const result = confirm("Are you here?")
-console.log(result);
+// const result = confirm("Are you here?")
+// console.log(result);
 
-const answer = prompt('Вам есть 18?', 'Да');
-console.log(answer);
+// const answer = prompt('Вам есть 18?', 'Да');
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('what is you name?', '');
+answers[1] = prompt('what is you last name?', '');
+answers[2] = prompt('How old are you', '');
+
+document.write(answers);
